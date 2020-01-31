599b4ddbec4cae4306dab93fd3ce95320461f27e
Trevor Mackin <tismetrm@gmail.com>
2020-01-30 17:02:54 -0800

    Add JWT Authorization

---
+++ JWTAuthentication/Startup.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace JWTAuthentication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+             // ----------------- This was added to configure for JWT authentication (Ref: R1) -----------------
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    // You can specify the values for the issuer, audience, and signing key. 
+                    // These values are stored in appsettings.json (Ref: E2)
+
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+
+            // ------------------------------------ End of (Ref: R2) ---------------------------------------
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // This is added to make the authentication service available to the application (Ref: R3) 
+            app.UseAuthentication();
+            // --------------------------------- End of (Ref:R2) ------------------------------------
+
+            app.UseMvc();
+        }
+    }
+}
+
+
+
+
